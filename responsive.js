51e3848269504265f86b629a67406545d0ac2a02
Matt Kersley <mkersley@gmail.com>
2011-07-18 13:52:19 +0100

    updated default URL to mine

--- responsive.js
+++ responsive.js
@@ -1,4 +1,4 @@
-var defaultURL = 'mediaqueri.es'; //<---- CHANGE TO YOUR WEBSITE URL
+var defaultURL = 'mattkersley.com'; //<---- CHANGE TO YOUR WEBSITE URL
 
 //show loading graphic
 function showLoader(id) {
