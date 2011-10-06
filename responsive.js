4695fca955389c0687e93ec93dbbf980bb6c8ad8
Eddie Monge <eddie@eddiemonge.com>
2011-10-05 17:19:03 -0700

    Redo js to be faster and cleaner

--- responsive.js
+++ responsive.js
@@ -1,110 +1,56 @@
-var defaultURL = 'mattkersley.com'; //<---- CHANGE TO YOUR WEBSITE URL
+(function() {
 
-//show loading graphic
-function showLoader(id) {
-  $('#' + id + ' img').fadeIn('slow');
-}
+var defaultURL = 'http://mattkersley.com';
 
-//hdie loading graphic
-function hideLoader(id) {
-  $('#' + id + ' img').fadeOut('slow');
-}
-  
-//function to check load state of each frame
-function allLoaded(){
-  var results = [];
-  $('iframe').each(function(){
-    if(!$(this).data('loaded')){results.push(false)}
-  });
-  var result = (results.length > 0) ? false : true;
-  return result;
-};
-
-function loadPage($frame, url) {
-  if(url.substr(0,7) != 'http://'){
-    url = 'http://'+url;
+// Load the url into the requested iframe
+function loadPage(url, frame) {
+	// If the url does not start with http, https, or file, then add http
+	if ( url.substr(0,4) !== 'http' && url.substr(0,5) !== 'https' && url.substr(0,4) !== 'file' ) {
+		// If the url somehow ended up as http:////, fix it.
+		url = ('http://'+url).replace('////', '//');
   }
-  $('iframe').not($frame).each(function(){showLoader($(this).parent().attr('id'));})
-  $('iframe').not($frame).data('loaded', false);
-  $('iframe').not($frame).attr('src', url);
-}
 
-$('.frame').each(function(){showLoader($(this).attr('id'))});  
-
-
-//when document loads
-$(document).ready(function(){
-  
-  loadPage('', defaultURL);
-  
-  //query string
-  var qsArray = window.location.href.split('?');
-  var qs = qsArray[qsArray.length-1];
-  
-  if(qs != '' && qsArray.length > 1){    
-    $('#url input[type=text]').val(qs);
-    loadPage('', qs);
-  }
-  
-  //set slidable div width
-  $('#frames #inner').css('width', function(){
-    var width = 0;
-    $('.frame').each(function(){width += $(this).outerWidth() + 20});
-    return width;
-  });
-  
-  //add event handlers for options radio buttons
-  $('input[type=radio]').change(function(){
-    $frames = $('#frames');
-    $inputs = $('input[type=radio]:checked').val();
-    
-    if($inputs == '1'){
-      $frames.addClass('widthOnly');
-    } else {
-      $frames.removeClass('widthOnly');
-    }
-  });
-  
-  //when the url textbox is used
-  $('form').submit(function(){
-    loadPage('' , $('#url input[type=text]').val());
-    return false;
-  });
-  
-  //when frame loads
-  $('iframe').load(function(){
-    
-    var $this = $(this);
-    var url = '';
-    var error = false;
-    
-    try{
-      url = $this.contents().get(0).location.href;
+	// Grab all the iframes. If an iframe changes, change the others as well
+	$('iframe').not(frame).attr('src', url).load(function() {
+		// Update the window hash to accurately portray whats being shown
+		document.location.hash = url;
+		var u;  
+		    try{
+      u = $this.contents().get(0).location;
     } catch(e) {
-      error = true;
-      if($('#url input[type=text]').val() != ''){
-        url = $('#url input[type=text]').val();
-      } else {
-        url = defaultURL;
-      }
     }
-  
-    //load other pages with the same URL
-    if(allLoaded()){
-      if(error){
-        alert('Browsers prevent navigation from inside iframes across domains.\nPlease use the textbox at the top for external sites.');
-        loadPage('', defaultURL);
-      }else{
-        loadPage($this, url);
-      }
-    }
-    
-    //when frame loads, hide loader graphic
-    else{
-      error = false;
-      hideLoader($(this).parent().attr('id'));
-      $(this).data('loaded',true);
-    }
+
+
+		console.log( u );
+	});
+}
+
+
+$(document).ready(function(){
+  // Grab the hash string, if there is one
+	var query = document.location.hash.substr(1) || false;
+   
+	// Load the initial page query. Start with the query string and fallback to the default
+  if ( query ) {    
+		$('#urlarea').val(query);
+    loadPage(query);
+  }
+	else {
+		loadPage(defaultURL);
+	}
+
+	// Toggle between emulated device widths and fixed height
+  $('#accurate').change(function() {
+		$('#frames').toggleClass('widthOnly');
   });
-  
+
+	// Fake the form submission. Grab the value and load that into the iframes
+  $('form').submit(function(e) {
+		var url = $('#urlarea').val();
+		e.preventDefault();
+    loadPage( url );
+  });
+	
 });
+
+})();
